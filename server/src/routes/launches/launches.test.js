16d1b7ba54acbc45ccf3c23d04daa64f98d5ba34
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-10-08 10:02:06 +0400

    fixed a type of a function

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
-const { loadPlantesData } = require('../../models/planets.model');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
    await mongoConnect();
-   await loadPlantesData();
+   await loadPlanetsData();
   });
 
   afterAll(async () => {
